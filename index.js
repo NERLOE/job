4f9bf4fd6ae325fbb1748554065c56c324f9d6db
NERLOE <marcusnerloe@gmail.com>
2019-11-11 23:11:05 +0100

    added server

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Alle forespørgelser, som ikke er blevet brugt ovenover, bliver sendt til React.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+  console.log(`Serveren åbnede på porten: ${port}`);
+});
